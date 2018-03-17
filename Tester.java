e7f1d41cf53a716cf5844fc5aada9c6b0cd7e271
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-03-17 18:01:19 +0000

    Finish second problem

--- Tester.java
+++ Tester.java
@@ -5,4 +5,6 @@ public class Tester
   public static void main(String args[])
   {
    System.out.println(d1: 3 * double PI * double sin(double 187) + double cos(double 122)); //d1 = -0.618672237585067
-   
+   System.out.println(d2: double pow(14.72, double 3.801) + double log(double 72)); //d2 = 27496.988867001543
+  }
+ }
