ee4f05e5f4c1a4ae7b74f0ec9893343119605b8c
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-03-17 17:54:32 +0000

    Complete first problem

---
+++ Tester.java
@@ -0,0 +1,8 @@
+//Author: Jacob Liddicoat
+//Date created: Mar 17,2018
+public class Tester
+ {
+  public static void main(String args[])
+  {
+   System.out.println(d1: 3 * double PI * double sin(double 187) + double cos(double 122)); //d1 = -0.618672237585067
+   
